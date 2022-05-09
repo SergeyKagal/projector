48f0b21981ce579608ac93a6d9a40c04e0602615
SergeyKagal <3461995@gmail.com>
2022-05-10 01:03:14 +0300

    feat: add context

--- src/Components/app/App.tsx
+++ src/Components/app/App.tsx
@@ -1,21 +1,26 @@
 import './app.scss';
-
 import SignIn from '../signin/SignIn';
 import SignUp from '../signup/SignUp';
 import { Route, Routes } from 'react-router-dom';
 import Welcome from '../welcome/Welcome';
 import Main from '../main/Main';
 import { BASE_URL, MAIN_ROUTE, SIGN_IN, SIGN_UP } from '../../constants/paths';
+import { GlobalContext } from '../../provider/provider';
+import { useState } from 'react';
 
 function App() {
+  const [isUserSignIn, setUserState] = useState(true);
+
   return (
     <div className="app">
-      <Routes>
-        <Route path={BASE_URL} element={<Welcome />} />
-        <Route path={MAIN_ROUTE} element={<Main />} />
-        <Route path={SIGN_IN} element={<SignIn />} />
-        <Route path={SIGN_UP} element={<SignUp />} />
-      </Routes>
+      <GlobalContext.Provider value={{ isUserSignIn, setUserState }}>
+        <Routes>
+          <Route path={BASE_URL} element={<Welcome />} />
+          <Route path={MAIN_ROUTE} element={<Main />} />
+          <Route path={SIGN_IN} element={<SignIn />} />
+          <Route path={SIGN_UP} element={<SignUp />} />
+        </Routes>
+      </GlobalContext.Provider>
     </div>
   );
 }
