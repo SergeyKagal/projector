d556ad9954fd74da3324d5ee772da9ee1a784eb6
SergeyKagal <3461995@gmail.com>
2022-05-11 23:35:30 +0300

    fix: file naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './Components/App/App';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
