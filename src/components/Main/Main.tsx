3e42bbc4e57d305aea0ac45bc99ce8afc5cb80a7
SergeyKagal <3461995@gmail.com>
2022-05-11 23:57:27 +0300

    fix: delete bad api request from main, update gitignore

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,19 +1,6 @@
-import axios from 'axios';
 import { Header } from '../Header/Header';
-import { API_URL } from '../../constants/paths';
 
 const Main = () => {
-  const getBoards = async () => {
-    await axios.post(`${API_URL}/boards`).then((res) => {
-      if (res.data) {
-        console.log(res.data);
-      }
-      return res.data;
-    });
-  };
-
-  getBoards();
-
   return (
     <>
       <Header></Header>
