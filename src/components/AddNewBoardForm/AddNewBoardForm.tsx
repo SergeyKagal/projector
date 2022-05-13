47bbcdf1c2c5792c179d1f6341cf444088ba3cee
Elena_Shashina <Elena_Shashina@epam.com>
2022-05-13 17:09:07 +0300

    feat: complete implementing AddNewBoardForm

--- src/components/AddNewBoardForm/AddNewBoardForm.tsx
+++ src/components/AddNewBoardForm/AddNewBoardForm.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useFormik } from 'formik';
@@ -10,6 +9,7 @@ import { addBoard, getBoards } from '../../api/api';
 import { Board } from '../../constants/interfaces';
 import { token } from '../../constants/mockValues';
 import { GlobalContext } from '../../provider/provider';
+import './AddNewBoardForm.scss';
 
 const AddNewBoardForm = (props: { setBoardsArray: (array: Board[]) => void }) => {
   const { setIsAddBoardFormOpen } = useContext(GlobalContext);
@@ -24,17 +24,18 @@ const AddNewBoardForm = (props: { setBoardsArray: (array: Board[]) => void }) =>
 
   const validationSchema = Yup.object({
     title: Yup.string()
-      .min(3, 'The title must be between 3 and 40 characters.')
-      .max(40, 'The title must be between 3 and 20 characters.')
+      .min(3, 'The title must be between 3 and 30 characters.')
+      .max(30, 'The title must be between 3 and 30 characters.')
       .required('This field is required!'),
   });
 
   const addNewBoard = async (formValue: IState) => {
     const { title } = formValue;
+
     await addBoard(token, title);
+
     const newArray = await getBoards(token);
     props.setBoardsArray(newArray);
-
     setIsAddBoardFormOpen(false);
   };
 
@@ -45,54 +46,40 @@ const AddNewBoardForm = (props: { setBoardsArray: (array: Board[]) => void }) =>
   });
 
   return (
-    <Container
-      maxWidth="xs"
-      sx={{
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContext: 'center',
-        position: 'fixed',
-        top: '0px',
-        left: '0px',
-      }}
-    >
-      <form onSubmit={formik.handleSubmit}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContext: 'space-between',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Add title for new board
-          </Typography>
-          <Box sx={{ px: 0, py: 2 }}>
-            <TextField
-              sx={{ mt: 2 }}
-              fullWidth
-              id="title"
-              name="title"
-              label="title"
-              type="title"
-              value={formik.values.title}
-              onChange={formik.handleChange}
-              error={formik.touched.title && Boolean(formik.errors.title)}
-              helperText={formik.touched.title && formik.errors.title}
-            />
-          </Box>
-          <Button variant="outlined" onClick={() => setIsAddBoardFormOpen(false)}>
+    <div className="addNewBoard__container">
+      <form onSubmit={formik.handleSubmit} className="addNewBoard__form">
+        <Typography component="h1" variant="h5">
+          Add title for new board
+        </Typography>
+        <Box sx={{ width: '75%', px: 0, py: 2 }}>
+          <TextField
+            sx={{ mt: 2 }}
+            fullWidth
+            id="title"
+            name="title"
+            label="title"
+            type="title"
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            error={formik.touched.title && Boolean(formik.errors.title)}
+            helperText={formik.touched.title && formik.errors.title}
+          />
+        </Box>
+        <Box sx={{ width: '75%', px: 0, py: 2, display: 'flex', justifyContent: 'center' }}>
+          <Button
+            variant="outlined"
+            onClick={() => setIsAddBoardFormOpen(false)}
+            sx={{ margin: '0 10px' }}
+          >
             Cancel
           </Button>
-          <Button type="submit" variant="contained">
+          <Button type="submit" variant="contained" sx={{ margin: '0 10px' }}>
             Add board
           </Button>
         </Box>
       </form>
       {/* <Notification /> */}
-    </Container>
+    </div>
   );
 };
 
