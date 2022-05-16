3c16230feb74c19005d9077049b8be5ad0a39fc1
Elena_Shashina <Elena_Shashina@epam.com>
2022-05-16 18:33:23 +0300

    feat: start implementing colorful header for Column

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -2,126 +2,47 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
-import { IBoard, IColumn } from '../../constants/interfaces';
+import { IColumn } from '../../constants/interfaces';
 import './Column.scss';
-import { FocusEvent, useState } from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import CheckIcon from '@mui/icons-material/Check';
-import CloseIcon from '@mui/icons-material/Close';
-import { getBoardById, updateColumn } from '../../api/api';
 
 interface IColumnProps {
-  board: IBoard;
   column: IColumn;
+  color: string;
   setColumnToDelete: (column: IColumn) => void;
   setShowConfirmPopUp: (flag: boolean) => void;
-  setBoard: (board: IBoard) => void;
-}
-
-interface IState {
-  title: string;
 }
 
 const Column = (props: IColumnProps) => {
-  const [editTitleMode, setEditTitleMode] = useState(false);
-
-  const handleClick = () => {
-    props.setColumnToDelete(props.column);
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    column: IColumn
+  ) => {
+    event.stopPropagation();
+    props.setColumnToDelete(column);
     props.setShowConfirmPopUp(true);
   };
 
-  const editColumnTitle = async (formValue: IState) => {
-    const { title } = formValue;
-    const updatedColumn = { ...props.column, title: title };
-
-    await updateColumn(props.board.id, updatedColumn);
-    props.setBoard(await getBoardById(props.board.id));
-    setEditTitleMode(false);
-  };
-
-  const validationSchema = Yup.object({
-    title: Yup.string()
-      .min(3, 'Min 3 char')
-      .max(15, 'Max 15 char')
-      .required('This field is required!'),
-  });
-
-  const formik = useFormik({
-    initialValues: { title: props.column.title },
-    validationSchema: validationSchema,
-    onSubmit: editColumnTitle,
-  });
-
-  const handleTitleClick = () => {
-    setEditTitleMode(true);
-  };
-
-  const handleCancelEditTitle = () => {
-    formik.resetForm();
-    setEditTitleMode(false);
-  };
-
-  const handleBlur = (
-    event: FocusEvent<HTMLTextAreaElement | HTMLInputElement, Element>,
-    formikBlur: (e: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>) => void
-  ) => {
-    setEditTitleMode(false);
-    formikBlur(event);
+  const styles = {
+    backgroundColor: props.color,
   };
 
   return (
-    <Container className="column">
-      <div className="title-container">
-        <div className="column__title" onClick={() => handleTitleClick()}>
-          {props.column.title}
-        </div>
-        <Button sx={{ p: '0px', minWidth: '' }} onClick={() => handleClick()}>
+    <Container className='column'>
+      <div className='column__header' style={styles}></div>
+      <div className='column__title'>
+        {props.column.title}
+        <Button
+          sx={{ p: '0px', minWidth: '' }}
+          onClick={(event) => handleClick(event, props.column)}
+        >
           {<DeleteIcon />}
         </Button>
-
-        {editTitleMode && (
-          <div className="title-edit">
-            <form onSubmit={formik.handleSubmit} className="title-edit__form">
-              <Box sx={{ p: 0, display: 'flex', justifyContent: 'center' }}>
-                <TextField
-                  fullWidth
-                  size="small"
-                  id="title"
-                  name="title"
-                  value={formik.values.title}
-                  autoFocus
-                  onChange={formik.handleChange}
-                  onBlur={(e) => handleBlur(e, formik.handleBlur)}
-                  error={formik.touched.title && Boolean(formik.errors.title)}
-                  helperText={formik.touched.title && formik.errors.title}
-                />
-                <Button
-                  aria-label="submit"
-                  type="submit"
-                  variant="contained"
-                  sx={{ width: '32px', height: '32px', minWidth: 0, boxShadow: 'none' }}
-                >
-                  <CheckIcon />
-                </Button>
-
-                <Button
-                  aria-label="delete"
-                  variant="outlined"
-                  sx={{ width: '32px', height: '32px', minWidth: 0, p: '6px 16px' }}
-                  onClick={handleCancelEditTitle}
-                >
-                  <CloseIcon />
-                </Button>
-              </Box>
-            </form>
-          </div>
-        )}
       </div>
-
-      <Button variant="text" className="button-add-item" startIcon={<AddIcon />}>
+      <Button
+        variant='text'
+        className='button-add-item'
+        startIcon={<AddIcon />}
+      >
         ADD TASK
       </Button>
     </Container>
