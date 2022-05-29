4586bd4c887b30902ffaae0869279b400534e628
Elena Shashina <elvehnn@yandex.ru>
2022-05-29 20:45:38 +0300

    refactor: refactor footer adaptive styles

--- src/components/Copyright/Copyright.tsx
+++ src/components/Copyright/Copyright.tsx
@@ -3,21 +3,46 @@ import Typography from '@mui/material/Typography';
 
 const Copyright = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="right">
-      Copyright ©
-      <Link color="inherit" href="https://github.com/SergeyKagal" target="_blank">
-        Sergey Kagal
-      </Link>
-      , ©
-      <Link color="inherit" href="https://github.com/ravgusha" target="_blank">
-        Ravganiyat Suleymanova
-      </Link>
-      , ©
-      <Link color="inherit" href="https://github.com/Elvehnn" target="_blank">
-        Elena Shashina
-      </Link>
-      , 2022.
-    </Typography>
+    <div>
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="right"
+        sx={{ display: { xs: 'none', md: 'block' } }}
+      >
+        Copyright ©
+        <Link color="inherit" href="https://github.com/SergeyKagal" target="_blank">
+          Sergey Kagal
+        </Link>
+        , ©
+        <Link color="inherit" href="https://github.com/ravgusha" target="_blank">
+          Ravganiyat Suleymanova
+        </Link>
+        , ©
+        <Link color="inherit" href="https://github.com/Elvehnn" target="_blank">
+          Elena Shashina
+        </Link>
+        , 2022.
+      </Typography>
+
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="right"
+        sx={{ display: { xs: 'block', md: 'none' } }}
+      >
+        <Link color="inherit" href="https://github.com/SergeyKagal" target="_blank">
+          ©Sergey
+        </Link>
+        <Link color="inherit" href="https://github.com/ravgusha" target="_blank">
+          , ©Ravganiyat
+        </Link>
+        <Link color="inherit" href="https://github.com/Elvehnn" target="_blank">
+          , ©Elena
+        </Link>
+        , 2022.
+      </Typography>
+    </div>
   );
 };
 
